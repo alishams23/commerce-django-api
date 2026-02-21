41cbc91599e5a9b9a7a83043bfbc09731d39ee8c
M-A-Rahimi <marahimi750@gmail.com>
2026-02-21 19:26:11 +0330

    Enable multi-select filtering for colors and brands
    - Updated ProductFilter to support selecting multiple colors and brands simultaneously.
    - Implemented BaseInFilter with lookup_expr='in' to handle comma-separated values.
    - Adjusted ProductsListView queryset with .distinct() to prevent duplicate products due to JOINs.
    - API now supports queries like `?color=2,3&brand=1,5` for OR-style filtering.

--- product/filters.py
+++ product/filters.py
@@ -1,16 +1,18 @@
 import django_filters
-
+from django_filters import rest_framework as filters
 from product.models import Product
 
+class NumberInFilter(filters.BaseInFilter, filters.NumberFilter):
+    pass
 
 class ProductFilter(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(field_name="fixed_price", lookup_expr="gte")
 
     max_price = django_filters.NumberFilter(field_name="fixed_price", lookup_expr="lte")
 
-    brand = django_filters.NumberFilter(field_name="brand__id")
+    brand = NumberInFilter(field_name="brand__id")
 
-    color = django_filters.NumberFilter(field_name="colors__color__id")
+    color = NumberInFilter(field_name="colors__color__id",lookup_expr = 'in')
 
     class Meta:
         model = Product
