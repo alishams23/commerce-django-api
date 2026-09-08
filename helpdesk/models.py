84dcdfc935dbfd219c0ef9d8fb07ffce8b87e30c
MRahimi-i <marahimi750@gmail.com>
2026-09-08 10:53:51 +0330

    Fix: change verbose name from en to fa

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -3,27 +3,30 @@ from core.models.auditable import AuditableModel
 from core.models.soft_delete import SoftDeleteModel
 from helpdesk.utils import generate_ticket_number
 from user.models import User
-from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 
 class Department(AuditableModel, SoftDeleteModel):
-    name = models.CharField(max_length=100, unique=True, verbose_name=_("Name"))
-    is_active = models.BooleanField(default=True, verbose_name=_("Is active"))
+    name = models.CharField(max_length=100, unique=True, verbose_name=("نام دپارتمان"))
+    is_active = models.BooleanField(default=True, verbose_name=("فعال/غیرفعال"))
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = ("دپارتمان")
+        verbose_name_plural = ("دپارتمان ها")
+
 
 class Ticket(AuditableModel, SoftDeleteModel):
     class Priority(models.IntegerChoices):
-        LOW = 1, _("Low")
-        MEDIUM = 2, _("Medium")
-        HIGH = 3, _("High")
+        LOW = 1, ("کم")
+        MEDIUM = 2, ("متوسط")
+        HIGH = 3, ("زیاد")
 
     class Status(models.IntegerChoices):
-        OPEN = 1, _("Open")
-        CLOSED =0, _("Closed")
+        OPEN = 1, ("باز")
+        CLOSED =0, ("بسته")
 
     # user = created_by
 
@@ -33,17 +36,17 @@ class Ticket(AuditableModel, SoftDeleteModel):
         related_name="assigned_tickets",
         null=True,
         blank=True,
-        verbose_name=_("Assigned to"),
+        verbose_name=("ارجاع داده شده به"),
     )
 
     department = models.ForeignKey(
         Department,
         on_delete=models.PROTECT,
         related_name="tickets",
-        verbose_name=_("Department"),
+        verbose_name=("دپارتمان"),
     )
 
-    title = models.CharField(max_length=255, verbose_name=_("Title"))
+    title = models.CharField(max_length=255, verbose_name=("عنوان"))
 
     ticket_number = models.PositiveIntegerField(
         blank = True,
@@ -51,31 +54,32 @@ class Ticket(AuditableModel, SoftDeleteModel):
         unique=True,
         editable=False,
         db_index=True,
+        verbose_name=("شماره تیکت"),
     )
 
     reference_code = models.CharField(
         max_length=100,
         null=True,
         blank=True,
-        verbose_name=_("Reference code"),
+        verbose_name=("رفرنس کد"),
     )
 
     priority = models.SmallIntegerField(
         choices=Priority.choices,
         default=Priority.MEDIUM,
         db_index=True,
-        verbose_name=_("Priority"),
+        verbose_name=("اولویت"),
     )
     status = models.SmallIntegerField(
         choices=Status.choices,
         default=Status.OPEN,
         db_index=True,
-        verbose_name=_("Status"),
+        verbose_name=("وضعیت"),
     )
 
     class Meta:
-        verbose_name = _("Ticket")
-        verbose_name_plural = _("Tickets")
+        verbose_name = ("تیکت")
+        verbose_name_plural = ("تیکت ها")
         ordering = ["-status","priority","-created_at"]
         indexes = [
             models.Index(fields=["created_by", "status"]),
@@ -97,7 +101,7 @@ class Ticket(AuditableModel, SoftDeleteModel):
         
 
     def __str__(self):
-        return f"Ticket #{self.ticket_number}" if self.ticket_number else "New Ticket"
+        return f"تیکت #{self.ticket_number}" if self.ticket_number else "تیکت جدید"
 
 
 class TicketMessage(AuditableModel, SoftDeleteModel):
@@ -105,18 +109,18 @@ class TicketMessage(AuditableModel, SoftDeleteModel):
         Ticket,
         on_delete=models.CASCADE,
         related_name="messages",
-        verbose_name=_("Ticket"),
+        verbose_name=("تیکت"),
     )
 
-    message = models.TextField(verbose_name=_("Message"))
+    message = models.TextField(verbose_name=("پیام"))
 
     is_admin = models.BooleanField(
-        default=False, db_index=True, verbose_name=_("Is admin")
+        default=False, db_index=True, verbose_name=("ارسال شده توسط ادمین")
     )
 
     class Meta:
-        verbose_name = _("Ticket message")
-        verbose_name_plural = _("Ticket messages")
+        verbose_name = "پیام تیکت"
+        verbose_name_plural = "پیام‌های تیکت"
         ordering = ["-created_at"]
         indexes = [
             models.Index(fields=["ticket", "created_at"]),
@@ -124,4 +128,4 @@ class TicketMessage(AuditableModel, SoftDeleteModel):
         ]
 
     def __str__(self):
-        return f"Message #{self.pk} for Ticket #{self.ticket_id}"
+        return f"پیام #{self.pk} برای تیکت #{self.ticket_id}"
